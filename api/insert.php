fa04ea1cc40d47f0e83af375147b2b85aa2feeb4
oliviawu <oliviawu0301@gmail.com>
2024-12-06 16:40:02 +0800

    新增資料庫

--- api/insert.php
+++ api/insert.php
@@ -1,16 +1,18 @@
 <?php
 include_once "db.php";
 
+$table=$_POST['table'];
+// ucfirst => 變成大寫
+$db=ucfirst($table);
+
 
 if(!empty($_FILES['img']['tmp_name'])){
-
     move_uploaded_file($_FILES['img']['tmp_name'],"../upload/".$_FILES['img']['name']);
     $_POST['img']=$_FILES['img']['name'];
 }
 
+unset($_POST['table']);
+$$db->save($_POST);
 
-$Title->save($_POST);
 
-
-
-to("../admin.php?do=title");
+to("../admin.php?do=$table");

d44bd2e98deb41294ab33173bcf822c61a371e02
oliviawu <oliviawu0301@gmail.com>
2024-12-09 10:46:24 +0800

    完成管理者管理增改刪查功能

--- api/insert.php
+++ api/insert.php
@@ -6,6 +6,18 @@ if(!empty($_FILES['img']['tmp_name'])){
     move_uploaded_file($_FILES['img']['tmp_name'],"../upload/".$_FILES['img']['name']);
     $_POST['img']=$_FILES['img']['name'];
 }
+
+// 確認密碼可以不需要儲存
+
+
+
+
 unset($_POST['table']);
+
+if(isset($_POST['pw2'])){
+    unset($_POST['pw2']);
+
+}
+
 $$db->save($_POST);
 to("../admin.php?do=$table");
--- backend/admin.php
+++ backend/admin.php
@@ -31,7 +31,7 @@
                             <input type="text" name="acc[]" value="<?=$row['acc'];?>">
                         </td>
                         <td>
-                            <input type="text" name="pw[]" value="<?=$row['pw'];?>">
+                            <input type="password" name="pw[]" value="<?=$row['pw'];?>">
                         </td>
                         <td>
                             <input type="checkbox" name="del[]" value="<?=$row['id'];?>">

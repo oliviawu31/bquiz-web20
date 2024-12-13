9942b7ccfb8ab5f510cba334ca6c075d69bf8df2
oliviawu <oliviawu0301@gmail.com>
2024-12-13 10:42:02 +0800

    使用彈出視窗完成進站人數功能

--- admin.php
+++ admin.php
@@ -52,9 +52,11 @@
                         <div class="mainmu">
                             校園映象資料管理 </div>
                     </a>
-                    <a style="color:#000; font-size:13px; text-decoration:none;" href="?do=total">
-                        <div class="mainmu">
-                            進站總人數管理 </div>
+                    <a style="color:#000; font-size:13px; text-decoration:none;" href="#"
+                        onclick="op('#cover','#cvr','./modal/view.php')">
+                        <div class=" mainmu">
+                            進站總人數管理
+                        </div>
                     </a>
                     <a style="color:#000; font-size:13px; text-decoration:none;" href="?do=bottom">
                         <div class="mainmu">
